c5bf30ad39af6f8a0063a9f17c1fd65a8580eab6
Vasiliy <91281754+vasyasgors@users.noreply.github.com>
2025-10-18 16:15:29 +0300

    исправлен баг с мерцанием примитивов если не очищать экран

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFML.Learning
+{
+    class Program : Game
+    {
+        static void Main()
+        {
+               
+            InitWindow(800, 600, "Meow");
+            SetWindowMode(WindowMode.Resizable);
+
+            int px = MouseX, py = MouseY;
+
+            while (true)
+            {
+                // 1. Расчет
+                DispatchEvents();
+
+                // Игровая логика
+
+                // 2. Очистка буфера и окна
+                //ClearWindow();
+
+                // 3. Отрисовка буфера на окне
+
+                if (GetMouseButton(0) == true)
+                    FillRectangle(MouseX, MouseY,  100, 100);
+
+
+                
+                // Вызов методов отрисовки объектов
+
+                DisplayWindow();
+
+                px = MouseX;
+                py = MouseY;
+
+                // 4. Ожидание
+                //Delay(1);
+            }
+        }
+    }
+}
+
